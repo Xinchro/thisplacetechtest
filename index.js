d3d02d819c58acff18a9df215514cf1e43cdcd63
Xinchro <TheXrocks@gmail.com>
2018-02-06 12:09:09 +0000

    made setname into a function so that nodemon stops firing it on every save

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const fetch = require("node-fetch")
 
-fetch("http://dev-challenge.thisplace.com/hello", {
-  "method": "post",
-  "body": JSON.stringify({
-    "name": "Walter Reis"
-  }),
-  "headers": {
-    "Content-Type": "application/json"
-  }
-})
-.then(res => res.text())
-.then(body => console.log(body))
+function setName() {
+  fetch("http://dev-challenge.thisplace.com/hello", {
+    "method": "post",
+    "body": JSON.stringify({
+      "name": "Walter Reis"
+    }),
+    "headers": {
+      "Content-Type": "application/json"
+    }
+  })
+  .then(res => res.text())
+  .then(body => console.log(body))
+}
+
