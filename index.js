7a70689bba3ddd974c64dc11dc244044364ee126
Xinchro <TheXrocks@gmail.com>
2018-02-06 12:23:22 +0000

    little refactoring on question object, changed name to single name to avoid complications and did question 1

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const fetch = require("node-fetch")
 
 
 const baseURL = "http://dev-challenge.thisplace.com"
-const questionURLs = {
-  "first": "/question/1/Walter Reis/d5202403"
+const quests = {
+  "one": {
+    "method": "GET",
+    "url": "/question/1/Walter/309363b7"
+  },
+  "two": {
+    "method": "POST",
+    "url": "/question/1/Walter/309363b7"
+  }
 }
-
 
 function setName() {
   fetch(`${baseURL}/hello`, {
     "method": "post",
     "body": JSON.stringify({
-      "name": "Walter Reis"
+      "name": "Walter"
     }),
     "headers": {
       "Content-Type": "application/json"
@@ -20,3 +26,12 @@ function setName() {
   .then(res => res.text())
   .then(body => console.log(body))
 }
+
+function question1() {
+  fetch(`${baseURL}${quests.one.url}`)
+  .then(res => res.text())
+  .then(body => console.log(body))
+}
+
+// setName()
+// question1()
