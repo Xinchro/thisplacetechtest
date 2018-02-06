a2046ec085ea8cf18ade0a7baf066d8b1af8ce74
Xinchro <TheXrocks@gmail.com>
2018-02-06 12:31:07 +0000

    did a little bit of refactoring to have a single question method

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
 const fetch = require("node-fetch")
 
-
 const baseURL = "http://dev-challenge.thisplace.com"
+
 const quests = {
+  "name": {
+    "method": "POST",
+    "url": "/hello",
+    "body": JSON.stringify({
+      "name": "Walter"
+    })
+  },
   "one": {
     "method": "GET",
     "url": "/question/1/Walter/309363b7"
   },
   "two": {
     "method": "POST",
-    "url": "/question/1/Walter/309363b7"
+    "url": "/question/1/Walter/309363b7",
+    "body": ""
   }
 }
 
-function setName() {
-  fetch(`${baseURL}/hello`, {
-    "method": "post",
-    "body": JSON.stringify({
-      "name": "Walter"
-    }),
-    "headers": {
-      "Content-Type": "application/json"
-    }
-  })
-  .then(res => res.text())
-  .then(body => console.log(body))
+function doQuestion(url, method, body) {
+  if(method === "POST") {
+    fetch(`${baseURL}${url}`, {
+      "method": "post",
+      "body": body,
+      "headers": {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => res.text())
+    .then(body => console.log(body))
+  } else
+  if(method === "GET") {
+    fetch(`${baseURL}${url}`)
+    .then(res => res.text())
+    .then(body => console.log(body))
+  }
 }
 
-function question1() {
-  fetch(`${baseURL}${quests.one.url}`)
-  .then(res => res.text())
-  .then(body => console.log(body))
-}
-
-// setName()
-// question1()
+// question 1
+// doQuestion(quests.one.url, quests.one.method, quests.one.data)
